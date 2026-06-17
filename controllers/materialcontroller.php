58e70e7540f5d611d9154d1f894c1c496730a3db
Mazen Ahmed <68770331+MazenPlus0@users.noreply.github.com>
2026-06-18 01:23:21 +0300

    materialcontroller.php

--- controllers/materialcontroller.php
+++ controllers/materialcontroller.php
@@ -37,3 +37,45 @@ function deleteMaterial($id)
         "Material Deleted Successfully"
     );
 }
+
+//add material
+function addMaterial(){
+    $data = json_decode(
+        file_get_contents("php://input"),
+        true
+    );
+    addMaterialRepo(
+        $data["session_id"],
+        $data["content"]
+    );
+    response(
+        201,
+        "Material Added Successfully"
+    );
+}
+
+//update Material
+function updateMaterial($id){
+    $material = getMaterialByIdRepo($id);
+
+    if(!$material){
+        response(
+            404,
+            "Material Not Found"
+        );
+        return;
+    }
+    $data = json_decode(
+    file_get_contents("php://input"),
+    true
+    );
+    updateMaterialRepo(
+        $id,
+        $data["content"]
+    );
+    response(
+        200,
+        "Material Updated Successfully"
+    );
+}
+?>
